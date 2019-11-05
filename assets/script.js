976f2c3ebc8ea211221f2acbdf942afc3d5504fa
alic3579 <pany468@newschool.edu>
2019-11-05 02:19:40 -0500

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+fetch('/assets/my.json')
+.then(function(results){
+	return results.json()
+})
+.then(function(jsonResults){
+
+	console.log("results: ", jsonResults)
+
+	document.querySelector("#root").insertAdjacentHTML('afterbegin', `
+		<h1 style="color:rgb(${jsonResults.color});">${jsonResults.title}</h1> 
+	`)
+
+})
+.catch(function(error){
+	console.log("error message:", error)
+})
+
+
+fetch('https://api.kanye.rest/')
+.then(function(results){
+	return results.json()
+})
+.then(function(jsonResults){
+
+	console.log("results: ", jsonResults)
+
+
+})
+.catch(function(error){
+	console.log("error message:", error)
+})
